2d6281ddd3485c3a1121d237208ec387c0809341
Sankar Ragothaman <sankarragothaman@Sankars-MacBook-Pro.local>
2020-07-02 16:16:47 +0400

    initial commit after integrating gihub with visual studio code

---
+++ 01.py
@@ -0,0 +1,22 @@
+import math
+import os
+import sys
+
+import numpy as num
+import requests
+
+print(sys.version)
+print(sys.executable)
+print("numpyversion", num.version.version)
+
+
+def greet(who_to_greet):
+    greeting = "Hello {}".format(who_to_greet)
+    return greeting
+
+
+print(greet("Hridya"))
+print(greet("Arjuna"))
+
+r = requests.get('http://www.google.com')
+print(r.status_code)
